bf00339dcccab998efef4355a3dcb49a53ab5931
OyaYagiz <oyagiz93@gmail.com>
2025-05-12 00:21:14 +0300

    feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { useContext } from "react";
 import SideBar from "../components/SideBar";
+import { VideoContext } from "../context/videoContext";
 
 const Feed = () => {
+  const { videos, error, isLoading } = useContext(VideoContext);
   return (
     <div className="flex gap-10">
       <SideBar />
