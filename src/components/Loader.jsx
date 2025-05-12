e47b5d90da73ec768e99435577387e74c8b759ea
OyaYagiz <oyagiz93@gmail.com>
2025-05-13 01:52:32 +0300

    skeleton structure was established

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { HiUserCircle } from "react-icons/hi2";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4];
+
+  return arr.map(() => (
+    <div className="p-4 rounded shadow animate-pulse md:p-6">
+      <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-700" />
+
+      <div className="flex items-center mt-4 gap-3">
+        <HiUserCircle className="text-5xl text-gray-700" />
+        <div>
+          <div className="h-2.5 rounded-full bg-gray-700 w-44" />
+          <div className="w-16 h-2  rounded-full bg-gray-700 my-3" />
+          <div className="flex gap-2">
+            <div className="w-28 h-2 bg-gray-700 rounded-full " />
+            <div className="w-28 h-2 bg-gray-700 rounded-full " />
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
 };
 
 export default Loader;
